b5ba3b860fc128f12ff58b67c752919897e88742
Ian Qvist <iqv@agilresponse.com>
2019-10-12 21:00:51 +0200

    Fix naming of AwsRegion

--- src/SimpleS3.Abstracts/Enums/AwsRegion.cs
+++ src/SimpleS3.Abstracts/Enums/AwsRegion.cs
@@ -12,82 +12,82 @@ namespace Genbox.SimpleS3.Abstracts.Enums
 
         /// <summary>Asia Pacific (Hong Kong)</summary>
         [EnumMember(Value = "ap-east-1")]
-        APEast1,
+        ApEast1,
 
         /// <summary>Asia Pacific (Tokyo)</summary>
         [EnumMember(Value = "ap-northeast-1")]
-        APNorthEast1,
+        ApNorthEast1,
 
         /// <summary>Asia Pacific (Seoul)</summary>
         [EnumMember(Value = "ap-northeast-2")]
-        APNorthEast2,
+        ApNorthEast2,
 
         /// <summary>Asia Pacific (Osaka-Local)</summary>
         [EnumMember(Value = "ap-northeast-3")]
-        APNorthEast3,
+        ApNorthEast3,
 
         /// <summary>Asia Pacific (Mumbai)</summary>
         [EnumMember(Value = "ap-south-1")]
-        APSouth1,
+        ApSouth1,
 
         /// <summary>Asia Pacific (Singapore)</summary>
         [EnumMember(Value = "ap-southeast-1")]
-        APSouthEast1,
+        ApSouthEast1,
 
         /// <summary>Asia Pacific (Sydney)</summary>
         [EnumMember(Value = "ap-southeast-2")]
-        APSouthEast2,
+        ApSouthEast2,
 
         /// <summary>Canada (Central)</summary>
         [EnumMember(Value = "ca-central-1")]
-        CACentral1,
+        CaCentral1,
 
         /// <summary>China (Beijing)</summary>
         [EnumMember(Value = "cn-north-1")]
-        CNNorth1,
+        CnNorth1,
 
         /// <summary>China (Ningxia)</summary>
         [EnumMember(Value = "cn-northwest-1")]
-        CNNorthWest1,
+        CnNorthWest1,
 
         /// <summary>EU (Frankfurt)</summary>
         [EnumMember(Value = "eu-central-1")]
-        EUCentral1,
+        EuCentral1,
 
         /// <summary>EU (Stockholm)</summary>
         [EnumMember(Value = "eu-north-1")]
-        EUNorth1,
+        EuNorth1,
 
         /// <summary>EU (Ireland)</summary>
         [EnumMember(Value = "eu-west-1")]
-        EUWest1,
+        EuWest1,
 
         /// <summary>EU (London)</summary>
         [EnumMember(Value = "eu-west-2")]
-        EUWest2,
+        EuWest2,
 
         /// <summary>EU (Paris)</summary>
         [EnumMember(Value = "eu-west-3")]
-        EUWest3,
+        EuWest3,
 
         /// <summary>South America (Sao Paulo)</summary>
         [EnumMember(Value = "sa-east-1")]
-        SAEast1,
+        SaEast1,
 
         /// <summary>US East (N. Virginia)</summary>
         [EnumMember(Value = "us-east-1")]
-        USEast1,
+        UsEast1,
 
         /// <summary>US East (Ohio)</summary>
         [EnumMember(Value = "us-east-2")]
-        USEast2,
+        UsEast2,
 
         /// <summary>US West (N. California)</summary>
         [EnumMember(Value = "us-west-1")]
-        USWest1,
+        UsWest1,
 
         /// <summary>US West (Oregon)</summary>
         [EnumMember(Value = "us-west-2")]
-        USWest2
+        UsWest2
     }
 }
