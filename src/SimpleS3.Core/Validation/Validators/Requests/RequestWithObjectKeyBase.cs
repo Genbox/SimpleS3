e545d6b8f0fa53935caa26a50393bdc74daf820a
Ian Qvist <iqv@agilresponse.com>
2019-10-27 20:53:49 +0100

    Refactor a couple of validators

---
+++ src/SimpleS3.Core/Validation/Validators/Requests/RequestWithObjectKeyBase.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Genbox.SimpleS3.Core.Requests;
+using Microsoft.Extensions.Options;
+
+namespace Genbox.SimpleS3.Core.Validation.Validators.Requests
+{
+    public abstract class RequestWithObjectKeyBase<T> : BaseRequestValidator<T> where T : BaseRequest
+    {
+        protected RequestWithObjectKeyBase(IOptions<S3Config> config) : base(config)
+        {
+            RuleFor(x => x.ObjectKey).NotEmpty().WithMessage("You must provide an object key.");
+        }
+    }
+}
