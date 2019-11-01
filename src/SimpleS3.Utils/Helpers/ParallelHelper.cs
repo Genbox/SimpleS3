0d651c52eee2525618fd6b9ec99ff85f1ffb4899
Ian Qvist <iqv@agilresponse.com>
2019-11-01 22:44:49 +0100

    Add a helper to do parallel tasks

---
+++ src/SimpleS3.Utils/Helpers/ParallelHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Genbox.SimpleS3.Utils.Helpers
+{
+    public static class ParallelHelper
+    {
+        public static async Task<T[]> PerformInParallel<T>(IEnumerable<Task<T>> tasks, int concurrent = 10)
+        {
+            Queue<Task<T>> queue = new Queue<Task<T>>();
+
+            using (SemaphoreSlim semaphore = new SemaphoreSlim(concurrent))
+            {
+                foreach (Task<T> task in tasks)
+                {
+                    await semaphore.WaitAsync().ConfigureAwait(false);
+
+                    queue.Enqueue(PerformTask<T>(task, semaphore));
+                }
+
+                await Task.WhenAll(queue).ConfigureAwait(false);
+            }
+
+            return queue.Select(x => x.Result).ToArray();
+        }
+
+        public static async Task PerformInParallel(IEnumerable<Task> tasks, int concurrent = 10)
+        {
+            Queue<Task> queue = new Queue<Task>();
+
+            using (SemaphoreSlim semaphore = new SemaphoreSlim(concurrent))
+            {
+                foreach (Task task in tasks)
+                {
+                    await semaphore.WaitAsync().ConfigureAwait(false);
+
+                    queue.Enqueue(PerformTask(task, semaphore));
+                }
+
+                await Task.WhenAll(queue).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task PerformTask(Task task, SemaphoreSlim semaphore)
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        private static async Task<T> PerformTask<T>(Task<T> task, SemaphoreSlim semaphore)
+        {
+            try
+            {
+                return await task.ConfigureAwait(false);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
