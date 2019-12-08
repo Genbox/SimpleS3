2d466f8338bd868f90be6bf4123fbd8246fe5210
Ian Qvist <iqv@agilresponse.com>
2019-12-08 16:05:38 +0100

    Fix bug in CreateBucket

--- src/SimpleS3.Core/Internal/Marshal/Request/Bucket/CreateBucketRequestMarshal.cs
+++ src/SimpleS3.Core/Internal/Marshal/Request/Bucket/CreateBucketRequestMarshal.cs
@@ -2,7 +2,6 @@ using System.IO;
 using Genbox.SimpleS3.Abstracts;
 using Genbox.SimpleS3.Abstracts.Constants;
 using Genbox.SimpleS3.Abstracts.Marshal;
-using Genbox.SimpleS3.Core.Internal.Extensions;
 using Genbox.SimpleS3.Core.Internal.Helpers;
 using Genbox.SimpleS3.Core.Internal.Xml;
 using Genbox.SimpleS3.Core.Network.Requests.Buckets;
@@ -15,7 +14,9 @@ namespace Genbox.SimpleS3.Core.Internal.Marshal.Request.Bucket
     {
         public Stream MarshalRequest(CreateBucketRequest request, IS3Config config)
         {
-            request.AddHeader(AmzHeaders.XAmzBucketObjectLockEnabled, request.EnableObjectLocking);
+            //We only enable object locking on creation. We can't disable it, so there is no "false" option
+            if (request.EnableObjectLocking.HasValue && request.EnableObjectLocking.Value)
+                request.AddHeader(AmzHeaders.XAmzBucketObjectLockEnabled, request.EnableObjectLocking.Value ? "TRUE" : string.Empty);
 
             //Hard-code the LocationConstraint to the region from the config
             FastXmlWriter writer = new FastXmlWriter(128);
