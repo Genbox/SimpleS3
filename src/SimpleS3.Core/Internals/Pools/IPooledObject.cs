71319a50212b51daf7d3a29bde0d694a314ce2ce
Ian Qvist <iqv@agilresponse.com>
2020-01-26 13:38:42 +0100

    Add object pool

---
+++ src/SimpleS3.Core/Internals/Pools/IPooledObject.cs
@@ -0,0 +1,7 @@
+﻿namespace Genbox.SimpleS3.Core.Internals.Pools
+{
+    internal interface IPooledObject
+    {
+        void Reset();
+    }
+}
---
+++ src/SimpleS3.Core/Internals/Pools/ObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Globalization;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Genbox.SimpleS3.Core.Internals.Pools
+{
+    internal class ObjectPool<T> where T : IPooledObject
+    {
+        private readonly int _maxCapacity;
+        private readonly ConcurrentBag<T> _pool;
+
+        public static readonly ObjectPool<T> Shared = new ObjectPool<T>();
+
+        public ObjectPool(int maxCapacity = 32)
+        {
+            _maxCapacity = maxCapacity;
+            _pool = new ConcurrentBag<T>();
+        }
+
+        public int Count => _pool.Count;
+
+        public T Rent(Action<T> setup)
+        {
+            if (!_pool.TryTake(out T obj))
+                obj = (T)Activator.CreateInstance(typeof(T), BindingFlags.NonPublic | BindingFlags.Instance, null, null, CultureInfo.InvariantCulture);
+
+            setup(obj);
+            return obj;
+        }
+
+        public async Task<TReturn> RentAndUse<TReturn>(Action<T> setup, Func<T, Task<TReturn>> action)
+        {
+            T obj = Rent(setup);
+            TReturn returnVal = await action(obj).ConfigureAwait(false);
+            Return(obj);
+            return returnVal;
+        }
+
+        public void Return(T obj)
+        {
+            if (_pool.Count > _maxCapacity)
+                return;
+
+            //We reset here instead of in Rent() because it frees memory for strings and objects by releasing references.
+            obj.Reset();
+            _pool.Add(obj);
+        }
+    }
+}
