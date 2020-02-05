b8d184ae0dda453fdabb2a9f1f73acc660d0e292
Michael Bisbjerg <michael@mbwarez.dk>
2020-02-05 13:52:01 +0100

    Add tests for timeout
    WIP - not working yet

--- src/SimpleS3.Core.Tests/Code/Other/SlowHttpHandler.cs
+++ src/SimpleS3.Core.Tests/Code/Other/SlowHttpHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,17 +8,19 @@ using Genbox.SimpleS3.Core.Common;
 namespace Genbox.SimpleS3.Core.Tests.Code.Other
 {
     /// <summary>
-    /// HTTP handler that fails with transient HTTP errors, except for each N requests
+    /// HTTP handler that delays all request, except each N requests
     /// </summary>
-    internal class TransientFailingHttpHandler : BaseFailingHttpHandler
+    internal class SlowHttpHandler : BaseFailingHttpHandler
     {
         private readonly int _successRate;
+        private readonly TimeSpan _delay;
 
-        public TransientFailingHttpHandler(int successRate = 3)
+        public SlowHttpHandler(int successRate, TimeSpan delay)
         {
             Validator.RequireThat(successRate >= 1, nameof(successRate), "successRate must be greater than or equal 1");
 
             _successRate = successRate;
+            _delay = delay;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -30,8 +33,11 @@ namespace Genbox.SimpleS3.Core.Tests.Code.Other
                 return CreateResponse(request, HttpStatusCode.OK);
             }
 
-            // Transient error
-            return CreateResponse(request, HttpStatusCode.InternalServerError);
+            // Delayed
+            await Task.Delay(_delay, cancellationToken).ConfigureAwait(false);
+
+            // TODO: Return timeouts?
+            return CreateResponse(request, HttpStatusCode.OK);
         }
     }
 }
