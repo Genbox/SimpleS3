748872259a26965d19e616be623ee91d85e3583a
Ian Qvist <qvistian@gmail.com>
2020-10-02 00:44:20 +0200

    Skip duplicates in DotnetPush

--- DotnetPush.ps1
+++ DotnetPush.ps1
@@ -1,2 +1,2 @@
 $NugetKey = Read-Host "Enter Nuget key"
-dotnet nuget push -k $NugetKey -s https://api.nuget.org/v3/index.json TempRelease\*
+dotnet nuget push --skip-duplicate -k $NugetKey -s https://api.nuget.org/v3/index.json TempRelease\*
