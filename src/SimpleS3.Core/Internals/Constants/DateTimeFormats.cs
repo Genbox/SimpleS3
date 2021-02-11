fafeeff1c8f22c2cb4ba8c9a183a8e7ed37ac7fb
Ian Qvist <qvistian@gmail.com>
2021-02-11 21:37:34 +0100

    Fix bug in timezone

--- src/SimpleS3.Core/Internals/Constants/DateTimeFormats.cs
+++ src/SimpleS3.Core/Internals/Constants/DateTimeFormats.cs
@@ -3,8 +3,8 @@
     internal static class DateTimeFormats
     {
         public const string Iso8601Date = "yyyyMMdd";
-        public const string Iso8601DateTime = @"yyyyMMdd\THHmmss\Z";
-        public const string Iso8601DateTimeExtended = @"yyyy-MM-dd\THH:mm:ss.fff\Z";
+        public const string Iso8601DateTime = @"yyyyMMdd\THHmmssZ";
+        public const string Iso8601DateTimeExtended = @"yyyy-MM-dd\THH:mm:ss.fffZ";
         public const string Rfc1123 = "R";
     }
 }
