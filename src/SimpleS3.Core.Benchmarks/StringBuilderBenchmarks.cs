79f9437f61cdf7e1f226a1a5bc7d7565a1704970
Ian Qvist <qvistian@gmail.com>
2021-03-04 21:22:26 +0100

    Add a couple of string benchmarks

--- src/SimpleS3.Core.Benchmarks/StringBuilderBenchmarks.cs
+++ src/SimpleS3.Core.Benchmarks/StringBuilderBenchmarks.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using BenchmarkDotNet.Attributes;
+using Genbox.SimpleS3.Core.Common.Pools;
 
 namespace Genbox.SimpleS3.Core.Benchmarks
 {
@@ -8,24 +9,48 @@ namespace Genbox.SimpleS3.Core.Benchmarks
     {
         private readonly StringBuilder _append = new StringBuilder();
         private readonly StringBuilder _appendFormat = new StringBuilder();
+        public string EqualSign = "=";
 
-        [IterationCleanup]
-        public void IterationCleanup()
+        [Benchmark]
+        public string Append()
         {
             _append.Clear();
+            return _append.Append("MyKey").Append('=').Append("MyValue").ToString();
+        }
+
+        [Benchmark]
+        public string AppendFormat()
+        {
             _appendFormat.Clear();
+            return _appendFormat.AppendFormat("{0}={1}", "MyKey", "MyValue").ToString();
         }
 
         [Benchmark]
-        public StringBuilder Append()
+        public string NormalAdd()
         {
-            return _append.Append("MyKey").Append('=').Append("MyValue");
+            //We use a field here to avoid a smart compiler from creating a constant
+            return "MyKey" + EqualSign + "MyValue";
         }
 
         [Benchmark]
-        public StringBuilder AppendFormat()
+        public string Interpolation()
         {
-            return _appendFormat.AppendFormat("{0}={1}", "MyKey", "MyValue");
+            //We use a field here to avoid a smart compiler from creating a constant
+            return $"MyKey{EqualSign}MyValue";
+        }
+
+        [Benchmark]
+        public string FromPool()
+        {
+            StringBuilder sb = StringBuilderPool.Shared.Rent();
+
+            //We use a field here to avoid a smart compiler from creating a constant
+            sb.Append("MyKey").Append(EqualSign).Append("MyValue");
+
+            string val = sb.ToString();
+
+            StringBuilderPool.Shared.Return(sb);
+            return val;
         }
     }
 }
