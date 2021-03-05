5d70aec8a4158c908933bb5e5fe400a849d6048b
Ian Qvist <qvistian@gmail.com>
2021-03-05 14:30:05 +0100

    Always require setting the json input format

--- src/SimpleS3.Core/Network/Requests/S3Types/S3JsonInputFormat.cs
+++ src/SimpleS3.Core/Network/Requests/S3Types/S3JsonInputFormat.cs
@@ -4,6 +4,11 @@ namespace Genbox.SimpleS3.Core.Network.Requests.S3Types
 {
     public class S3JsonInputFormat : S3InputFormat
     {
+        public S3JsonInputFormat(JsonType jsonType)
+        {
+            JsonType = jsonType;
+        }
+
         public JsonType JsonType { get; set; }
 
         internal override void Reset()
