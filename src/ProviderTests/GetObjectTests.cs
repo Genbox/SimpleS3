3c89e290a43c0302f44eaa96e3804a058077a2fc
Ian Qvist <qvistian@gmail.com>
2021-10-03 23:26:10 +0200

    Try multiple provider data attribute

---
+++ src/ProviderTests/GetObjectTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Genbox.SimpleS3.Core.Abstracts;
+using Genbox.SimpleS3.Core.Extensions;
+using Genbox.SimpleS3.Core.Network.Responses.Objects;
+using Genbox.SimpleS3.Extensions.ProfileManager.Abstracts;
+using Genbox.SimpleS3.Utility.Shared;
+using Xunit;
+
+namespace Genbox.ProviderTests
+{
+    public sealed class MultipleProvidersAttribute : Xunit.Sdk.DataAttribute
+    {
+        private ISimpleClient? _currentClient = null;
+
+        public override IEnumerable<object?[]> GetData(System.Reflection.MethodInfo testMethod)
+        {
+            foreach (var (profile, client) in ProviderSetup.Instance.Clients)
+            {
+                yield return new object?[] { profile, client };
+            }
+        }
+
+        public override string? Skip => /*_currentClient == null ? "not supported" : */null;
+    }
+
+    public class GetObjectTests
+    {
+        [Theory]
+        [MultipleProviders]
+        public async Task GetObjectContentRange(IProfile profile, ISimpleClient client)
+        {
+            string bucketName = UtilityHelper.GetTestBucket(profile);
+
+            PutObjectResponse putResp = await client.PutObjectAsync(bucketName, nameof(GetObjectContentRange), null);
+            GetObjectResponse getResp = await client.GetObjectAsync(bucketName, nameof(GetObjectContentRange), req => req.Range.Add(0, 2)).ConfigureAwait(false);
+
+            Assert.Equal(206, getResp.StatusCode);
+            Assert.Equal(3, getResp.ContentLength);
+            Assert.Equal("bytes", getResp.AcceptRanges);
+            Assert.Equal("bytes 0-2/4", getResp.ContentRange);
+            Assert.Equal("tes", await getResp.Content.AsStringAsync().ConfigureAwait(false));
+        }
+    }
+}
---
+++ src/ProviderTests/ProviderSetup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Genbox.SimpleS3.AmazonS3.Extensions;
+using Genbox.SimpleS3.BackBlazeB2.Extensions;
+using Genbox.SimpleS3.Core.Abstracts;
+using Genbox.SimpleS3.Extensions.HttpClientFactory.Extensions;
+using Genbox.SimpleS3.Extensions.ProfileManager.Abstracts;
+using Genbox.SimpleS3.Extensions.ProfileManager.Extensions;
+using Genbox.SimpleS3.GoogleCloudStorage.Extensions;
+using Genbox.SimpleS3.ProviderBase.Abstracts;
+using Genbox.SimpleS3.Utility.Shared;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Genbox.ProviderTests
+{
+    public class ProviderSetup
+    {
+        private ProviderSetup()
+        {
+            BuildProvider(S3Provider.AmazonS3);
+            BuildProvider(S3Provider.BackBlazeB2);
+            BuildProvider(S3Provider.GoogleCloudStorage);
+        }
+
+        public static ProviderSetup Instance { get; } = new ProviderSetup();
+        public IList<(IProfile, ISimpleClient)> Clients { get; } = new List<(IProfile, ISimpleClient)>();
+
+        private void BuildProvider(S3Provider provider)
+        {
+            ServiceCollection services = new ServiceCollection();
+            IClientBuilder clientBuilder;
+
+            switch (provider)
+            {
+                case S3Provider.AmazonS3:
+                    clientBuilder = services.AddAmazonS3();
+                    break;
+                case S3Provider.BackBlazeB2:
+                    clientBuilder = services.AddBackBlazeB2();
+                    break;
+                case S3Provider.GoogleCloudStorage:
+                    clientBuilder = services.AddGoogleCloudStorage();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(provider), provider, null);
+            }
+
+            clientBuilder
+                .CoreBuilder
+                .UseProfileManager()
+                .BindConfigToProfile("TestSetup-" + provider);
+
+            clientBuilder.HttpBuilder.UseProxy(new WebProxy("http://127.0.0.1:8888"));
+
+            ServiceProvider p = services.BuildServiceProvider();
+
+            IProfileManager profileManager = p.GetRequiredService<IProfileManager>();
+
+            IProfile? profile = profileManager.GetProfile("TestSetup-" + provider);
+
+            if (profile == null)
+                throw new InvalidOperationException("Unable to find profile");
+
+            Clients.Add((profile, p.GetRequiredService<ISimpleClient>()));
+        }
+    }
+}
