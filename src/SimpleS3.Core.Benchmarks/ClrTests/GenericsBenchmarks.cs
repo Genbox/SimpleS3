d6b0ddf8a434738cdbdc614422f456e8e3cdf56b
Ian Qvist <qvistian@gmail.com>
2022-01-18 15:56:45 +0100

    Add a benchmark to show the difference between interface calling and generics

---
+++ src/SimpleS3.Core.Benchmarks/ClrTests/GenericsBenchmarks.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+
+namespace Genbox.SimpleS3.Core.Benchmarks.ClrTests
+{
+    [InProcess]
+    public class GenericsBenchmarks
+    {
+        public interface IRequest
+        {
+            bool DoWork();
+        }
+
+        public class TestRequest : IRequest
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public bool DoWork()
+            {
+                //Don't do anything
+                return true;
+            }
+        }
+
+        public static class Helper
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public static bool TrySomething(IRequest request)
+            {
+                return request.DoWork();
+            }
+
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public static bool TrySomethingGeneric<T>(T request) where T : IRequest
+            {
+                return request.DoWork();
+            }
+        }
+
+        private static readonly IRequest _request = new TestRequest();
+
+        [Benchmark]
+        public bool Interface()
+        {
+            return Helper.TrySomething(_request);
+        }
+
+        [Benchmark]
+        public bool Generic()
+        {
+            return Helper.TrySomethingGeneric(_request);
+        }
+    }
+}
