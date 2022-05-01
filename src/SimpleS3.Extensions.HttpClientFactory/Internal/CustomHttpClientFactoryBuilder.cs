5d2a07252dbb43d56eda08fce51e85b97ef966ce
Ian Qvist <qvistian@gmail.com>
2022-05-01 13:09:04 +0200

    Refactor customer http builder

--- src/SimpleS3.Extensions.HttpClientFactory/Internal/CustomHttpClientFactoryBuilder.cs
+++ src/SimpleS3.Extensions.HttpClientFactory/Internal/CustomHttpClientFactoryBuilder.cs
@@ -1,15 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+﻿using Genbox.SimpleS3.Core.Abstracts;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Genbox.SimpleS3.Extensions.HttpClientFactory.Internal;
 
-internal class CustomHttpClientFactoryBuilder : IHttpClientBuilder
+internal class CustomHttpClientFactoryBuilder : ServiceBuilderBase, IHttpClientBuilder
 {
-    public CustomHttpClientFactoryBuilder(IServiceCollection services)
-    {
-        Services = services;
-    }
-
-    public string Name { get; } = Options.DefaultName;
-    public IServiceCollection Services { get; }
+    public CustomHttpClientFactoryBuilder(IServiceCollection services, string? name = null) : base(services, name) { }
 }
