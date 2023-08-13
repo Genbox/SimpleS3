7547d217af1e32fb92b1e669cd75b0d07913b755
Ian Qvist <qvistian@gmail.com>
2023-08-13 18:48:59 +0200

    Rewrite ConsoleProfileSetup for readability

--- Src/SimpleS3.Extensions.ProfileManager/Setup/ConsoleProfileSetup.cs
+++ Src/SimpleS3.Extensions.ProfileManager/Setup/ConsoleProfileSetup.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using System.Text;
 using Genbox.SimpleS3.Core.Abstracts.Provider;
 using Genbox.SimpleS3.Core.Abstracts.Region;
+using Genbox.SimpleS3.Core.Common.Validation;
 using Genbox.SimpleS3.Extensions.ProfileManager.Abstracts;
 using Genbox.SimpleS3.Extensions.ProfileManager.Internal.Helpers;
 
@@ -29,29 +30,30 @@ public class ConsoleProfileSetup : IProfileSetup
         if (existingProfile != null)
             return existingProfile;
 
-        start:
-
-        string enteredKeyId = GetKeyId();
-        byte[] accessKey = GetAccessKey();
-        IRegionInfo region = GetRegion();
-
-        Console.WriteLine();
-        Console.WriteLine("Please confirm the following information:");
-        Console.WriteLine("Key id: " + enteredKeyId);
-        Console.WriteLine("Region: " + region.Code + " -- " + region.Name);
-        Console.WriteLine();
-
         ConsoleKey key;
+        string enteredKeyId;
+        byte[] accessKey;
+        IRegionInfo region;
 
         do
         {
-            Console.WriteLine("Is it correct? Y/N");
+            enteredKeyId = GetKeyId();
+            accessKey = GetAccessKey();
+            region = GetRegion();
 
-            key = Console.ReadKey(true).Key;
-        } while (key != ConsoleKey.Y && key != ConsoleKey.N);
+            Console.WriteLine();
+            Console.WriteLine("Please confirm the following information:");
+            Console.WriteLine("Key id: " + enteredKeyId);
+            Console.WriteLine("Region: " + region.Code + " -- " + region.Name);
+            Console.WriteLine();
 
-        if (key == ConsoleKey.N)
-            goto start;
+            do
+            {
+                Console.WriteLine("Is it correct? Y/N");
+
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Y && key != ConsoleKey.N);
+        } while (key == ConsoleKey.N);
 
         IProfile profile = _profileManager.CreateProfile(profileName, enteredKeyId, accessKey, region.Code, persist);
 
@@ -72,51 +74,42 @@ public class ConsoleProfileSetup : IProfileSetup
     private string GetKeyId()
     {
         string? enteredKeyId;
-        bool validKeyId = true;
 
         Console.WriteLine();
         Console.WriteLine("Enter your key id - Example: AKIAIOSFODNN7EXAMPLE");
 
-        do
+        while (true)
         {
-            if (!validKeyId)
-                Console.Error.WriteLine("Invalid key id. Try again.");
-
             enteredKeyId = Console.ReadLine();
 
-            if (!string.IsNullOrEmpty(enteredKeyId))
-                enteredKeyId = enteredKeyId.Trim();
-        } while (!(validKeyId = _inputValidator.TryValidateKeyId(enteredKeyId, out _, out _)));
+            if (_inputValidator.TryValidateKeyId(enteredKeyId, out _, out _))
+                break;
 
-        return enteredKeyId!;
+            Console.Error.WriteLine("Invalid key id. Try again.");
+        }
+
+        Validator.RequireNotNull(enteredKeyId);
+
+        return enteredKeyId.Trim();
     }
 
     private byte[] GetAccessKey()
     {
-        char[]? enteredAccessKey = null;
-        byte[]? utf8AccessKey = null;
-        bool validAccessKey = true;
+        char[]? enteredAccessKey;
+        byte[]? utf8AccessKey;
 
         Console.WriteLine();
         Console.WriteLine("Enter your access key - Example: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
 
-        do
+        while (true)
         {
-            if (!validAccessKey)
-            {
-                Console.Error.WriteLine("Invalid access key. Try again.");
-                Array.Clear(enteredAccessKey!, 0, enteredAccessKey!.Length);
-                Array.Clear(utf8AccessKey!, 0, utf8AccessKey!.Length);
-            }
-
             enteredAccessKey = ConsoleHelper.ReadSecret(40);
 
             //Now we trim any whitespace characters the user might have entered
-
             int end;
-            int start;
+            int start = 0;
 
-            for (start = 0; start < enteredAccessKey.Length;)
+            while (start < enteredAccessKey.Length)
             {
                 if (!char.IsWhiteSpace(enteredAccessKey[start]))
                     break;
@@ -144,7 +137,14 @@ public class ConsoleProfileSetup : IProfileSetup
             }
 
             utf8AccessKey = Encoding.UTF8.GetBytes(enteredAccessKey);
-        } while (!(validAccessKey = _inputValidator.TryValidateAccessKey(utf8AccessKey, out _, out _)));
+
+            if (_inputValidator.TryValidateAccessKey(utf8AccessKey, out _, out _))
+                break;
+
+            Console.Error.WriteLine("Invalid access key. Try again.");
+            Array.Clear(enteredAccessKey, 0, enteredAccessKey.Length);
+            Array.Clear(utf8AccessKey, 0, utf8AccessKey.Length);
+        }
 
         //Clear the access key from memory
         Array.Clear(enteredAccessKey, 0, enteredAccessKey.Length);
@@ -169,19 +169,20 @@ public class ConsoleProfileSetup : IProfileSetup
             counter++;
         }
 
-        start2:
-        string? enteredRegion = Console.ReadLine();
-
-        if (enteredRegion != null)
+        while (true)
         {
-            if (int.TryParse(enteredRegion, out int index) && index >= 0 && index <= counter)
-                return _regionConverter.GetRegion(index);
+            string? enteredRegion = Console.ReadLine();
 
-            if (validRegionId.Contains(enteredRegion))
-                return _regionConverter.GetRegion(enteredRegion);
+            if (enteredRegion != null)
+            {
+                if (int.TryParse(enteredRegion, out int index) && index >= 0 && index <= counter)
+                    return _regionConverter.GetRegion(index);
+
+                if (validRegionId.Contains(enteredRegion))
+                    return _regionConverter.GetRegion(enteredRegion);
+            }
+
+            Console.Error.WriteLine("Invalid region. Try again.");
         }
-
-        Console.Error.WriteLine("Invalid region. Try again.");
-        goto start2;
     }
 }
