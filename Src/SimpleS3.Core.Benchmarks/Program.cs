4b93851d2adc309e94918499cd59a1268947c9df
Ian Qvist <qvistian@gmail.com>
2024-08-24 17:48:27 +0200

    Benchmarks should not output reports

--- Src/SimpleS3.Core.Benchmarks/Program.cs
+++ Src/SimpleS3.Core.Benchmarks/Program.cs
@@ -1,6 +1,8 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Analysers;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
 
 namespace Genbox.SimpleS3.Core.Benchmarks;
 
@@ -8,9 +10,23 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        IConfig config = DefaultConfig.Instance
-                                      .AddJob(new Job(RunMode.Short, Job.InProcess))
-                                      .WithOption(ConfigOptions.DisableLogFile, true);
+        IConfig config = ManualConfig.CreateMinimumViable()
+                                     .AddJob(new Job(RunMode.Short, Job.InProcess))
+                                     .AddAnalyser(EnvironmentAnalyser.Default,
+                                         MinIterationTimeAnalyser.Default,
+                                         MultimodalDistributionAnalyzer.Default,
+                                         RuntimeErrorAnalyser.Default,
+                                         BaselineCustomAnalyzer.Default,
+                                         HideColumnsAnalyser.Default)
+                                     .AddValidator(BaselineValidator.FailOnError,
+                                         SetupCleanupValidator.FailOnError,
+                                         JitOptimizationsValidator.FailOnError,
+                                         RunModeValidator.FailOnError,
+                                         GenericBenchmarksValidator.DontFailOnError,
+                                         DeferredExecutionValidator.FailOnError,
+                                         ParamsAllValuesValidator.FailOnError,
+                                         ParamsValidator.FailOnError)
+                                     .WithOption(ConfigOptions.DisableLogFile, true);
 
         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
     }
