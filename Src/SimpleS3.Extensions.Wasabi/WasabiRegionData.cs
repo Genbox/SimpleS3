8e3f685e0c858d63e84e8f017c1ec3145e7951a5
Moritz Renker <mr@bohnen.it>
2024-10-08 10:42:33 +0200

    Added additional Wasabi Regions

--- Src/SimpleS3.Extensions.Wasabi/WasabiRegionData.cs
+++ Src/SimpleS3.Extensions.Wasabi/WasabiRegionData.cs
@@ -1,4 +1,4 @@
-﻿using Genbox.SimpleS3.Core.Abstracts.Region;
+using Genbox.SimpleS3.Core.Abstracts.Region;
 
 namespace Genbox.SimpleS3.Extensions.Wasabi;
 
@@ -12,9 +12,15 @@ public class WasabiRegionData : IRegionData
         yield return new RegionInfo(WasabiRegion.UsEast2, "us-east-2", "North America (North Virginia)");
         yield return new RegionInfo(WasabiRegion.UsCentral1, "us-central-1", "North America (Texas)");
         yield return new RegionInfo(WasabiRegion.UsWest1, "us-west-1", "North America (Oregon)");
+        yield return new RegionInfo(WasabiRegion.CaCentral1, "ca-central-1", "Canada (Central)");
         yield return new RegionInfo(WasabiRegion.EuCentral1, "eu-central-1", "Europe (Amsterdam)");
+        yield return new RegionInfo(WasabiRegion.EuCentral2, "eu-central-2", "Europe (Frankfurt)");
         yield return new RegionInfo(WasabiRegion.EuWest1, "eu-west-1", "Europe (London)");
+        yield return new RegionInfo(WasabiRegion.EuWest2, "eu-west-2", "Europe (Paris)");
+        yield return new RegionInfo(WasabiRegion.EuSouth1, "eu-south-1", "Europe (Milan)");
         yield return new RegionInfo(WasabiRegion.ApNorthEast1, "ap-northeast-1", "Asia Pacific (Tokyo)");
         yield return new RegionInfo(WasabiRegion.ApNorthEast2, "ap-northeast-2", "Asia Pacific (Osaka)");
+        yield return new RegionInfo(WasabiRegion.ApSouthEast1, "ap-southeast-1", "Asia Pacific (Singapore)");
+        yield return new RegionInfo(WasabiRegion.ApSouthEast2, "ap-southeast-2", "Asia Pacific (Sydney)");
     }
 }
