5ad1b4d76a1020b189b8a79033bcf9a2bedfa2df
Ian Qvist <qvistian@gmail.com>
2024-11-04 18:30:53 +0100

    Support that no region data is provided

--- Src/SimpleS3.Core.Common/RegionConverter.cs
+++ Src/SimpleS3.Core.Common/RegionConverter.cs
@@ -9,8 +9,11 @@ public class RegionConverter : IRegionConverter
     private readonly Dictionary<int, IRegionInfo> _enumMap = new Dictionary<int, IRegionInfo>();
     private readonly Dictionary<string, IRegionInfo> _stringMap = new Dictionary<string, IRegionInfo>(StringComparer.OrdinalIgnoreCase);
 
-    public RegionConverter(IRegionData data)
+    public RegionConverter(IRegionData? data = null) //If we are not provided with region data, we don't do any mapping
     {
+        if (data == null)
+            return;
+
         foreach (IRegionInfo regionInfo in data.GetRegions())
         {
             int intVal = (int)Convert.ChangeType(regionInfo.EnumValue, typeof(int), NumberFormatInfo.InvariantInfo);
