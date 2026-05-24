001b8234127d30c02e5c5cf3cd0327c9893b1a4b
Ian Qvist <qvistian@gmail.com>
2026-05-24 19:56:42 +0200

    Don't share context in polly

--- Src/SimpleS3.Extensions.HttpClientFactory.Polly/Internal/PollyHttpMessageHandler.cs
+++ Src/SimpleS3.Extensions.HttpClientFactory.Polly/Internal/PollyHttpMessageHandler.cs
@@ -4,12 +4,11 @@ namespace Genbox.SimpleS3.Extensions.HttpClientFactory.Polly.Internal;
 
 internal sealed class PollyHttpMessageHandler(IAsyncPolicy<HttpResponseMessage> policy) : DelegatingHandler
 {
-    private readonly Context _context = new Context();
     private readonly IAsyncPolicy<HttpResponseMessage> _policy = policy ?? throw new ArgumentNullException(nameof(policy));
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        return await _policy.ExecuteAsync((_, ct) => SendCoreAsync(request, ct), _context, cancellationToken).ConfigureAwait(false);
+        return await _policy.ExecuteAsync((_, ct) => SendCoreAsync(request, ct), new Context(), cancellationToken).ConfigureAwait(false);
     }
 
     private Task<HttpResponseMessage> SendCoreAsync(HttpRequestMessage request, CancellationToken cancellationToken) => base.SendAsync(request, cancellationToken);
