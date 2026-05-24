c2a8aaf66003f0f40a16b2156965ebc678a84381
Ian Qvist <qvistian@gmail.com>
2026-05-24 20:36:44 +0200

    Add cancellation to stream extensions

--- Src/SimpleS3.Core/Extensions/StreamExtensions.cs
+++ Src/SimpleS3.Core/Extensions/StreamExtensions.cs
@@ -5,23 +5,23 @@ namespace Genbox.SimpleS3.Core.Extensions;
 
 public static class StreamExtensions
 {
-    public static async Task<byte[]> AsDataAsync(this Stream stream)
+    public static async Task<byte[]> AsDataAsync(this Stream stream, CancellationToken token = default)
     {
         using MemoryStream ms = new MemoryStream();
-        await stream.CopyToAsync(ms).ConfigureAwait(false);
+        await stream.CopyToAsync(ms, 81920, token).ConfigureAwait(false);
 
         return ms.ToArray();
     }
 
-    public static async Task<string> AsStringAsync(this Stream stream, Encoding? encoding = null)
+    public static async Task<string> AsStringAsync(this Stream stream, Encoding? encoding = null, CancellationToken token = default)
     {
         encoding ??= Constants.Utf8NoBom;
 
-        byte[] data = await AsDataAsync(stream).ConfigureAwait(false);
+        byte[] data = await AsDataAsync(stream, token).ConfigureAwait(false);
         return encoding.GetString(data);
     }
 
-    public static async Task CopyToFileAsync(this Stream stream, string file)
+    public static async Task CopyToFileAsync(this Stream stream, string file, CancellationToken token = default)
     {
         string? dir = Path.GetDirectoryName(file);
 
@@ -29,6 +29,6 @@ public static class StreamExtensions
             Directory.CreateDirectory(dir);
 
         using FileStream fs = File.OpenWrite(file);
-        await stream.CopyToAsync(fs).ConfigureAwait(false);
+        await stream.CopyToAsync(fs, 81920, token).ConfigureAwait(false);
     }
 }
